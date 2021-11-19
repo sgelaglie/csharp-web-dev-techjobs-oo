0e1b33652ed415fd60e3bf1a2f457347292befcd
Seble <sgelaglie@yahoo.com>
2021-11-18 21:03:37 -0500

    Final

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -1,39 +1,8 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class Location
+    public class Location : JobField
     {
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }
-
-        public Location()
-        {
-            Id = nextId;
-            nextId++;
-        }
-
-        // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
-        // mylocation = new Location("philadelphia")
-        public Location(string val) : this()
-        {
-            Value = val;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Location location &&
-                   Id == location.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
-    }
+        public Location(string value) : base(value){ }
+    }                
 }
